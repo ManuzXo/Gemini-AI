3f2aa172541d9a52e7e430e21b508d331bb1f7d2
emanuele_rispoli_2712 <e.rispoli@umbracontrol.it>
2024-10-18 12:38:26 +0200

    fix TextArea e messaggi

--- src/FrontEnd/Prompt.ts
+++ src/FrontEnd/Prompt.ts
@@ -1,32 +1,39 @@
 import Gemini from "../Gemini/Gemini";
 import Message from "./Message";
 
-class Prompt{
-    private static m_response : boolean = true;
-    public static Init(){
+class Prompt {
+    private static m_response: boolean = true;
+    public static Init() {
         let _prompt = document.getElementById("prompt-text") as HTMLInputElement;
-        _prompt.addEventListener("change", async (_event)=>{
-            await this.EventPrompt(_event, _prompt);
+        _prompt.addEventListener("keydown", async (_event) => {
+            _event.stopPropagation();
+            console.log("keydown");
+            if (_event.key === "Enter" && !_event.shiftKey) {
+                await this.EventPrompt(_event, _prompt); 
+            } 
         });
+        
+        _prompt.addEventListener("input", async (_event) => {
+            _prompt.style.height = 'auto'; // Resetta l'altezza per calcolare il nuovo valore
+            _prompt.style.height = (_prompt.scrollHeight) + 'px'; // Imposta l'altezza in base al contenuto
+        });
+
         let _btnSend = document.getElementById("prompt-send") as HTMLElement;
-        _btnSend.addEventListener("click", async (_event)=>{
+        _btnSend.addEventListener("click", async (_event) => {
             await this.EventPrompt(_event, _prompt);
         });
     }
-    public static async EventPrompt(_event: Event, _prompt: HTMLInputElement)
-    {
+    public static async EventPrompt(_event: Event, _prompt: HTMLInputElement) {
         _event.stopPropagation();
-        if(this.m_response)
-        {
+        if (this.m_response) {
             this.m_response = false;
             await this.SendPrompt(_prompt);
             this.m_response = true;
         }
     }
-    public static async SendPrompt(_prompt: HTMLInputElement)
-    {
+    public static async SendPrompt(_prompt: HTMLInputElement) {
         let _msg = _prompt.value;
-        if(_msg === '')
+        if (_msg.length === 0)
             return;
         _prompt.value = '';
         Message.CreateMessageUser(_msg);
