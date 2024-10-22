e5ea17e1143dcfa3f2819ef1fa7d8de1990d804d
emanuele_rispoli_2712 <e.rispoli@umbracontrol.it>
2024-10-22 09:30:35 +0200

    aggiunta animazione inizio chat

--- src/FrontEnd/Prompt.ts
+++ src/FrontEnd/Prompt.ts
@@ -3,7 +3,9 @@ import { Alert, AlertType_e } from "./Alert";
 import Message from "./Message";
 
 class Prompt {
+    private static m_firstMessage: boolean = false;
     private static m_hasResponse: boolean = true;
+    private static m_previusMsg: string = "";
     public static Init() {
         let _prompt = document.getElementById("prompt-text") as HTMLInputElement;
         _prompt.removeAttribute("disabled");
@@ -12,8 +14,11 @@ class Prompt {
             if (_event.key === "Enter" && !_event.shiftKey) {
                 await this.EventPrompt(_event, _prompt);
             }
+            if (_event.key === "ArrowUp") {
+                if (this.m_previusMsg != "" && this.m_hasResponse)
+                    _prompt.value = this.m_previusMsg;
+            }
         });
-
         _prompt.addEventListener("input", async (_event) => {
             _prompt.style.height = 'auto'; // Resetta l'altezza per calcolare il nuovo valore
             _prompt.style.height = (_prompt.scrollHeight) + 'px'; // Imposta l'altezza in base al contenuto
@@ -27,6 +32,11 @@ class Prompt {
     public static async EventPrompt(_event: Event, _prompt: HTMLInputElement) {
         try {
             _event.stopPropagation();
+            if (!this.m_firstMessage) {
+
+                this.StartChattingAnimation();
+                this.m_firstMessage = true;
+            }
             if (this.m_hasResponse) {
                 this.m_hasResponse = false;
                 await this.SendPrompt(_prompt);
@@ -41,11 +51,17 @@ class Prompt {
         let _msg = _prompt.value;
         if (_msg.length === 0)
             return;
+        this.m_previusMsg = _msg;
         _prompt.value = '';
         await Message.CreateMessageUser(_msg);
         let _response = await Gemini.Request(_msg);
         await Message.CreateMessageGemini(_response);
         _prompt.value = '';
+    }
+    public static StartChattingAnimation() {
+        let _container = document.querySelector(".prompt-container") as HTMLElement;
+        _container.classList.remove("init");
+        _container.classList.add("start");
     }
 }
 export default Prompt;
