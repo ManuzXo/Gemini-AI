b7710f81aa61002a0f18797695d904818fc9d294
emanuele_rispoli_2712 <e.rispoli@umbracontrol.it>
2024-10-22 14:21:22 +0200

    aggiunti commenti

--- src/FrontEnd/Prompt.ts
+++ src/FrontEnd/Prompt.ts
@@ -10,8 +10,8 @@ class Prompt {
     public static Init() {
         let _prompt = document.getElementById("prompt-text") as HTMLInputElement;
         _prompt.removeAttribute("disabled");
-        if (DeviceDetection.isDesktopDevice()) {
 
+        if (DeviceDetection.isDesktopDevice()) {
             _prompt.addEventListener("keydown", async (_event) => {
                 _event.stopPropagation();
                 if (_event.key === "Enter" && !_event.shiftKey) {
@@ -21,6 +21,7 @@ class Prompt {
                     await this.EventPrompt(_event, _prompt);
                 }
                 if (_event.key === "ArrowUp") {
+                    //ripristina il messaggio se il box è vuoto, se ce stato un vecchio messaggio e che gemini abbia risposto
                     if (_prompt.value === '' && this.m_previusMsg != "" && this.m_hasResponse) {
                         _prompt.value = this.m_previusMsg;
                         _event.preventDefault();
@@ -28,11 +29,13 @@ class Prompt {
                 }
             });
         }
+        //aggiorna il text area        
         _prompt.addEventListener("input", async (_event) => {
-            _prompt.style.height = 'auto'; // Resetta l'altezza per calcolare il nuovo valore
-            _prompt.style.height = (_prompt.scrollHeight) + 'px'; // Imposta l'altezza in base al contenuto
+            _prompt.style.height = 'auto';
+            _prompt.style.height = (_prompt.scrollHeight) + 'px';
         });
 
+        //call back del pulsante invio
         let _btnSend = document.getElementById("prompt-send") as HTMLElement;
         _btnSend.addEventListener("click", async (_event) => {
             _event.stopPropagation();
@@ -42,15 +45,24 @@ class Prompt {
     public static async EventPrompt(_event: Event, _prompt: HTMLInputElement) {
         try {
             if (this.m_hasResponse) {
+
                 this.m_hasResponse = false;
+
                 let _msg = _prompt.value;
+                //se il msg che invia ha del contenuto lo manda senno skippa
                 if (_msg.length != 0) {
+                    //clear del textarea
                     _prompt.value = '';
+
+                    //se è il primo messaggio porta il prompt sotto
                     if (!this.m_firstMessage) {
                         this.StartChattingAnimation();
                         this.m_firstMessage = true;
                     }
+                    //salva il messaggio inviato per la funzione ArrowUp
                     this.m_previusMsg = _msg;
+
+                    //invia a gemini il messagio
                     await this.SendPrompt(_msg);
                 }
                 this.m_hasResponse = true;
